4999be09dbd787dfcbea6738a08a002b19f70ec9
Ubuntu <ubuntu@ip-10-114-11-209.ec2.internal>
2013-05-02 21:12:35 +0000

    percentages

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import tornado.web
 import tornado.options
 import logging
+import os
 
 import utils
 
@@ -24,8 +25,8 @@ class TopicHandler(tornado.web.RequestHandler):
 
 settings = {
     "debug": True,
-    "template_path": "templates/",
-    "static_path": "static/",
+    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+    "static_path": os.path.join(os.path.dirname(__file__),"static"),
 }
 
 application = tornado.web.Application(
